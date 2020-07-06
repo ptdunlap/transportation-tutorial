8e5db7f6b52958584b47c36b3742a53bdb11e083
Paul Dunlap <pauldunlap02@aol.com>
2020-07-05 17:00:52 -0700

    Fixing docker push

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-
-docker build -t bananaforscale/transportation .
-docker push bananaforscale/transportation
-
 # Build and push image
-#docker build -t bananaforscale/transportation:latest -t bananaforscale/transportation:$GIT_SHA .
-#echo PUSHING LATEST
-#docker push bananaforscale/transportation:latest
-#echo PUSHING $GIT_SHA
-#docker push bananaforscale/transportation:$GIT_SHA
+docker build -t bananaforscale/transportation:latest -t bananaforscale/transportation:$GIT_SHA .
+docker push bananaforscale/transportation:latest
+docker push bananaforscale/transportation:$GIT_SHA
 
 # Start up deployments and services
-#kubectl apply -f kube/transportation
+kubectl apply -f kube/transportation
 
 # Deploy specific version
-#kubectl set image deployments/server-deployment transportation=bananaforscale/transportation:$GIT_SHA
+kubectl set image deployments/server-deployment transportation=bananaforscale/transportation:$GIT_SHA
